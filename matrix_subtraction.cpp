45c65fb2605a68db4f6cb51cd8e9d97e452d07f9
YUVRAJ SHAH <yurajshah555@gmail.com>
2026-08-18 09:25:16 +0530

    Implement matrix subtraction functionality
    This program prompts the user to enter two matrices and performs subtraction if they have the same dimensions.

---
+++ matrix_subtraction.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int m,n;
+  cout<<"Enter m : ";
+  cin>>m;
+  cout<<"Enter n : ";
+  cin>>n;
+  int arr[m][n];
+  for(int i=0; i<m; i++){
+    for(int j=0; j<n; j++){
+      cin>>arr[i][j];
+    }
+  }
+
+  int p,q;
+  cout<<"Enter p : ";
+  cin>>p;
+  cout<<"Enter q : ";
+  cin>>q;
+  int brr[p][q];
+  for(int i=0; i<p; i++){
+    for(int j=0; j<q; j++){
+      cin>>brr[i][j];
+    }
+  }
+
+  if(m!=p || n!=q){
+    cout<<"Subtraction not possible";
+    return -1;
+  }
+
+  for(int i=0; i<m; i++){
+    for(int j=0; j<n; j++){
+      cout<<arr[i][j] - brr[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+}
